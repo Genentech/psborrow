de2165b7375d841a1a2aee5ab7139e48ce4123d2
Secrest, Matt {MDBW~South San Francisco} <secrest.matthew@gene.com>
2022-04-06 18:53:59 -0700

    Revert "reverting quiet test"
    This reverts commit 86aa63b3d20d936cefecd0b4d7a69d79b87be9be.

--- tests/testthat/test-quiet.R
+++ tests/testthat/test-quiet.R
@@ -73,8 +73,24 @@ get_sim_data <- function() {
         seed = 47
     )
 
+    res2 <- run_mcmc_p(
+      dt = sample_time,
+      set_prior(pred = "all", prior = "gamma", r0 = 1,  alpha = c(0, 0)),
+      n.chains = 2,
+      n.adapt = 100,
+      n.burn = 100,
+      n.iter = 200,
+      seed = 47,
+      n.cores = 2
+    )
+
     summ <- get_summary(res)
-    return(summ)
+    summ2 <- get_summary(res2)
+
+    return(list(
+      summ,
+      summ2
+    ))
 }
 
 
